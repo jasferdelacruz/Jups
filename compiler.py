6ae532e68837d90bc3d5ba50ef006a7f3c3e3701
Jasfer Dela Cruz <jasferdelacruz1217@gmail.com>
2024-12-10 17:25:21 +0800

    nice

--- compiler.py
+++ compiler.py
@@ -3,7 +3,7 @@ import sys
 #read arguments
 program_filepath = sys.argv[1]
 
-print(program_filepath)
+print("[CMD] Parsing")
 
 #Tokenize Program
 
@@ -13,33 +13,87 @@ with open (program_filepath, "r") as program_file:
     program_lines = [
         line.strip() 
             for line in program_file.readlines()]
-    program =[]
-    for line in program_lines:
-        parts = line.split(" ")
-        opcode = parts [0]
+program =[]
+for line in program_lines:
+    parts = line.split(" ")
+    opcode = parts [0]
         
-        #check for empty line
-        if opcode == "":
-            continue
+    #check for empty line
+    if opcode == "":
+        continue
 
-#store opcode token
-program.append(opcode)
+    #store opcode token
+    program.append(opcode)
 
-#handle each opcode
-if opcode == "PUSH":
-    number = int(parts[1])
-    program.append(number)
-elif opcode == "PRINT":
-    #PARSE STRING LITERAL
-    string_literal =''.join(parts[1:])[1:-1]
-    program.append(string_literal)
-elif opcode == "JUMP.EQ.0":
-    #READ LABEL
-    label = parts[1]
-    program.append(label)
-elif opcode == "JUMP.GT.0":
-    #READ LABEL
-    label = parts[1]
-    program.append(label)
+    #handle each opcode
+    if opcode == "PUSH":
+        number = int(parts[1])
+        program.append(number)
+    elif opcode == "PRINT":
+        #PARSE STRING LITERAL
+        string_literal =''.join(parts[1:])[1:-1]
+        program.append(string_literal)
+    elif opcode == "JUMP.EQ.0":
+        #READ LABEL
+        label = parts[1]
+        program.append(label)
+    elif opcode == "JUMP.GT.0":
+        #READ LABEL
+        label = parts[1]
+        program.append(label)
 
-print(program)
+'''
+Compile to assembly
+'''
+
+asm_filepath = program_filepath[:-4] + ".asm"
+out =open(asm_filepath, "w")
+
+out.write(""";-- header --
+bits 64
+default rel
+""")         
+
+out.write(""";-- variables --
+section .bss
+""")
+
+out.write(""";-- contants --
+section .data
+""")
+
+out.write(""";-- Entry Point--
+section .text
+global main
+extern ExitProcess
+extern printf
+extern scanf
+          
+
+main:
+\tPUSH rbp
+\tMOV rbp, rsp
+\tSUB rsp, 32
+""")
+
+ip = 0
+while ip < len(program):
+    opcode = program[ip]
+    ip += 1
+
+    if opcode.endswith(":"):
+        out.write(f";-- Label --\n")
+        out.write(f"{opcode}\n")
+    elif opcode == "PUSH":
+        number = program[ip]
+        ip += 1
+
+        out.write(f";-- PUSH --\n")
+        out.write(f"\tPUSH {number}\n")
+    elif opcode == "POP":
+        out.write(f";-- POP --\n")
+        out.write(f"\tPOP\n")
+
+
+out.close()
+
